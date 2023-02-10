b2d152b15cfe2959bdf7ef543179534e08d99185
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2023-02-10 00:07:52 +0000

    Added collisions with capsule cast - Player Script
    Added collisions with capsule cast - Player Script.
    Adjusted collision with objects with diagonal inputs by making them hug walls opposed to just stopping. Normalized values keep the speed from slowing down on diagonal wall collisions.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,8 +19,51 @@ public class Player : MonoBehaviour
         //translate the vector 2 input into vector 3
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        //use the inputs for moving the game object world position, using the translate vector 3
-        transform.position += moveDir * playerMoveSpeed * Time.deltaTime;
+        //creat a capsule cast to check for collisions. Set paremeters for capsule cast
+        float moveDistance = playerMoveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        //set up movement for only x or z if colliding with diagonal input
+        //player will hug the wall instead of stopping still, makes collisions much smoother
+        if (!canMove )
+        {
+            //cannot move towards moveDir
+
+            //Attempt only X movement (using normized so value will be 1 - If not then it will be 0.71 and slower than moving on the X axis only) 
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if(canMove) 
+            {
+                //Can only move on the X axis
+                moveDir = moveDirX;
+            } else
+            {
+                //Cannot move only on the X axis
+                
+                //Attempt only Z movement (using normized so value will be 1 - If not then it will be 0.71 and slower than moving on the Z axis only) 
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    //Can move only on the Z axis
+                    moveDir = moveDirZ;
+                } else
+                {
+                    //Cannot move in any direction
+                }
+            }
+        }
+
+        //if can move is true, then allow player to move
+        if (canMove)
+        {
+            //use the inputs for moving the game object world position, using the translate vector 3
+            transform.position += moveDir * moveDistance;
+        }
 
         //check if player is moving
         playerIsWalking = moveDir != Vector3.zero;
